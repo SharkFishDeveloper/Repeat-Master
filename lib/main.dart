e032c32a947ebeae3d397e24afadf25a301a6211
SharkFishDeveloper <116112291+SharkFishDeveloper@users.noreply.github.com>
2023-05-30 21:31:57 +0530

    some chNGE

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:repeat_master/features/home/view/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeScreen(),
     );
   }
 }
