43167f7929c3aa94ce097a6d587bcfd25414edea
SharkFishDeveloper <116112291+SharkFishDeveloper@users.noreply.github.com>
2023-05-30 21:38:21 +0530

    event

--- lib/features/home/bloc/home_event.dart
+++ lib/features/home/bloc/home_event.dart
@@ -2,3 +2,9 @@ part of 'home_bloc.dart';
 
 @immutable
 abstract class HomeEvent {}
+
+class AddTaskEvent extends HomeEvent {}
+
+class DeleteTaskEvent extends HomeEvent {}
+
+class SeeDetailsEvent extends HomeEvent {}
